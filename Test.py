e111436b05362b51d214fce374069ab980b3f84a
QMblC <kurtovnevg@gmail.com>
2024-04-22 12:03:17 +0500

    3213

--- Test.py
+++ Test.py
@@ -1,3 +1,20 @@
-import flask
+import mysql.connector
+from mysql.connector import Error
 
-print(123)
+def connect():
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='python_mysql',
+                                       user='root',
+                                       password='1234')
+        if conn.is_connected():
+            print('Connected to MySQL database')
+
+    except Error as e:
+        print(e)
+
+    finally:
+        conn.close()
+
+if __name__ == '__main__':
+    connect()
