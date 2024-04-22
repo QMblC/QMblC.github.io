dcb8431e539a73be489df4a345b13ef06786102a
QMblC <kurtovnevg@gmail.com>
2024-04-22 12:50:51 +0500

    312

--- Test.py
+++ Test.py
@@ -1,20 +1,52 @@
 import mysql.connector
 from mysql.connector import Error
 
-def connect():
-    try:
-        conn = mysql.connector.connect(host='localhost',
-                                       database='python_mysql',
-                                       user='root',
-                                       password='1234')
-        if conn.is_connected():
-            print('Connected to MySQL database')
+class UserTable:
+    def connect(self):
+        try:
+            self.connection = mysql.connector.connect(host='localhost',
+                                        database='python_mysql',
+                                        user='root',
+                                        password='6124HuKuTa')
+            if self.connection.is_connected():
+                
+                print('Connected to MySQL database')
+                return self.connection
 
-    except Error as e:
-        print(e)
+        except Error as e:
+            print(e)
+            return None
 
-    finally:
-        conn.close()
+    def create_user_table(self):
+        create_movies_table_query = """
+        CREATE TABLE users(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(100)
+        )
+        """
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(create_movies_table_query)
+                self.connection.commit()
+                print('created')
+        except Error as e:
+            print(e)
+
+    def delete_user_table(self):
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute("""DROP TABLE users""")
+                self.connection.commit()
+                print("deleted")
+        except Error as e:
+            print(e)
+
+    def insert_user(self):
+        query = "INSERT INTO users(id,name) "
+        pass
 
 if __name__ == '__main__':
-    connect()
+    table = UserTable()
+    table.connect()
+    table.create_user_table()
+    
